7518a0d73e75489a091abcbc77625fd2eb9edaa5
D'Yanni <dyanniham@gmail.com>
2023-11-24 14:04:45 -0700

    able to get days and interval in a good format

---
+++ Utils/Entry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.Utils
+{
+    public class Entry
+    {
+        public List<string> Days
+        { get; set; }
+
+        public string Interval
+        { get; set; }
+
+        public string output;
+
+        // test method for debugging
+        public void testEntry()
+        {
+            Debug.WriteLine("Selected Days:");
+            Debug.WriteLine("==============\n");
+            Debug.WriteLine(output);
+
+            Debug.WriteLine("\n\n");
+
+            Debug.WriteLine("Selected Interval:");
+            Debug.WriteLine("==============\n");
+            Debug.WriteLine(Interval);
+        }
+
+        public void formatDays()
+        {
+            output = string.Join(", ", Days);
+        }
+    }
+}
