5a0000ff935d4a0d27ce67911521dbbc24f45e04
D'Yanni <dyanniham@gmail.com>
2023-12-06 18:02:32 -0700

    current error handling

---
+++ Utils/MissingUnitException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.Utils
+{
+    internal class MissingUnitException : Exception
+    {
+        public MissingUnitException() { }
+        public MissingUnitException(string message) : base(message) { }
+        public MissingUnitException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
