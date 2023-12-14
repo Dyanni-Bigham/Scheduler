6d29c34a5a7c5cf94f07a4ad658fa16f4b3c5a2b
D'Yanni <dyanniham@gmail.com>
2023-12-13 17:01:48 -0700

    move exceptions to own dir

--- obj/Debug/net7.0-windows/Scheduler.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Scheduler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scheduler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecaefa1409c3d59a4f61de1ca6ec22a838766a26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scheduler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scheduler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
