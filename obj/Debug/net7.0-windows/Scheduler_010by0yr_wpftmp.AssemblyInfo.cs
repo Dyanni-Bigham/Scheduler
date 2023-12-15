1e5e24d2e5684ee71e64df0dfa1bb09b0c74a24a
D'Yanni <dyanniham@gmail.com>
2023-12-14 19:30:17 -0700

    able to write to a file

--- obj/Debug/net7.0-windows/Scheduler_010by0yr_wpftmp.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Scheduler_010by0yr_wpftmp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scheduler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b176cbc7ea5cd32a585402ab5d05eae5676e7da3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scheduler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scheduler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
