44bd9446944bb839808ba0fb86c4c2e34ed2641d
D'Yanni <dyanniham@gmail.com>
2024-05-14 21:04:43 -0700

    add icon ability

--- Utils/TimeHandling.cs
+++ Utils/TimeHandling.cs
@@ -19,26 +19,40 @@ namespace Scheduler.Utils
         public static void scheduleTask()
         {
             string[] days = getValues("config.json");
-            /*
-            Debug.WriteLine("Inside of schedule tasks here are the values to use.");
-            Debug.WriteLine(interval);
+            //Debug.WriteLine("Inside of the scheduleTask method");
+
+            //Debug.WriteLine("Inside of schedule tasks here are the values to use.");
+            //Debug.WriteLine(interval);
             Debug.WriteLine(string.Join(", ", days));
-            */
+            bool result = isInDays(days);
+            Debug.WriteLine("Is the current day in the range?");
+            Debug.WriteLine(result);
+            
 
             // logic for determining when to run task
         }
 
+        // This checks if the current day is in the days selected by the user
         private static bool isInDays(string[] days)
         {
             DayOfWeek currentDay = DateTime.Today.DayOfWeek;
+            Debug.WriteLine(currentDay);
 
             foreach (string day in days)
             {
-                if (currentDay.Equals(day))
-                { return true; }
+                DayOfWeek dayOfWeek;
+                if (Enum.TryParse(day, true, out dayOfWeek)) // converts day to DayofWeek
+                {
+                    if (dayOfWeek == currentDay) 
+                    {
+                        return true;
+                    }
+                }
+
             }
 
             return false;
+
         }
 
         private static string[] getValues(string fileName)
