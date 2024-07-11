50de18d429bee5b15c5d900811311dda2bd8f094
D'Yanni <dyanniham@gmail.com>
2024-07-10 20:50:06 -0700

    create edit window to edit json entries

---
+++ EditWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+
+namespace Scheduler
+{
+    /// <summary>
+    /// Interaction logic for EditWindow.xaml
+    /// </summary>
+    public partial class EditWindow : Window
+    {
+        public ObservableCollection<Utils.Entry> Schedules { get; set; }
+        public EditWindow()
+        {
+
+            InitializeComponent();
+            Schedules = new ObservableCollection<Utils.Entry>();
+
+            string currDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string configFilePath = System.IO.Path.Combine(currDirectory, "config.json");
+            string jsonData = File.ReadAllText(configFilePath);
+
+            var schedulesFromJson = JsonConvert.DeserializeObject<List<Utils.Entry>>(jsonData);
+
+            foreach (var schedule in schedulesFromJson)
+            {
+                Schedules.Add(schedule);
+            }
+
+            dataGrid.ItemsSource = Schedules;
+        }
+
+        private void SaveChanges()
+        {
+            string json = JsonConvert.SerializeObject(Schedules, Formatting.Indented);
+            string currDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string configFilePath = System.IO.Path.Combine(currDirectory, "config.json");
+            File.WriteAllText(configFilePath, json);
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveChanges();
+            MessageBox.Show("Changes saved successfully.");
+        }
+
+        private void DeleteButton_Click(object obj, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                Schedules.Remove((Utils.Entry)dataGrid.SelectedItem);
+            }
+        }
+    }
+}
---
+++ Utils/FileNameConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Scheduler.Utils
+{
+    public class FileNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameterm, CultureInfo culture)
+        {
+            if (value is string path)
+            {
+                return Path.GetFileName(path);            
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
